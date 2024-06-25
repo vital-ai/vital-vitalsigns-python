be8e2845cc04d2e7b8ecb8d6a441ef73328b02bc
Marc Hadfield <marc@vital.ai>
2024-06-25 11:26:05 -0400

    sync

---
+++ vital_ai_vitalservice_cmd/vitalservice_export_command.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import sys
+
+
+class VitalServiceExportCommand:
+    def __init__(self, args):
+        self.parser = self.create_parser()
+        self.args = self.parser.parse_args()
+        self.vital_home = os.getenv('VITAL_HOME', '')
+
+    def create_parser(self):
+
+        parser = argparse.ArgumentParser(description="VitalServiceExport Command", add_help=True)
+        subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+        help_parser = subparsers.add_parser('help', help="Display help information")
+
+        info_parser = subparsers.add_parser('info', help="Display information about the system and environment")
+
+        return parser
+
+    def run(self):
+        if self.args.command == 'help':
+            self.parser.print_help()
+        elif self.args.command == 'info':
+            self.info()
+        else:
+            self.parser.print_help()
+
+    def info(self):
+        vital_home = self.vital_home
+        print("VitalServiceExport Info")
+        print(f"Current VITAL_HOME: {vital_home}")
+
+
+def main():
+    import sys
+    command = VitalServiceExportCommand(sys.argv[1:])
+    command.run()
+
+
+if __name__ == "__main__":
+    main()
